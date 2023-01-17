6ff3da1688f0d04bf4b290c705969423b8a93ec6
rifa <rifadianekaf@gmail.com>
2023-01-17 14:24:58 +0700

    firebase authguard finish

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    firebase: {
+      apiKey: "AIzaSyD7eFl15HHx8UYdJj4lCRvSDPa33-ZKN0I",
+      authDomain: "fir-authguard.firebaseapp.com",
+      projectId: "fir-authguard",
+      storageBucket: "fir-authguard.appspot.com",
+      messagingSenderId: "529291880647",
+      appId: "1:529291880647:web:e56d4e88ed678b0ea076e2"
+    }
+  };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
