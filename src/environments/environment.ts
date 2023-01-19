9fc85151abe555b64907a66283e00cb8d2dd0ca5
rifa <rifadianekaf@gmail.com>
2023-01-19 15:16:18 +0700

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
     firebase: {
       apiKey: "AIzaSyD7eFl15HHx8UYdJj4lCRvSDPa33-ZKN0I",
       authDomain: "fir-authguard.firebaseapp.com",
