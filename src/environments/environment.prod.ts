2ad5109c7fb7466e4356c5a5f8b8e51648cf3947
rifa <rifadianekaf@gmail.com>
2023-01-19 15:11:14 +0700

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD7eFl15HHx8UYdJj4lCRvSDPa33-ZKN0I",
+    authDomain: "fir-authguard.firebaseapp.com",
+    databaseURL: "https://fir-authguard-default-rtdb.firebaseio.com",
+    projectId: "fir-authguard",
+    storageBucket: "fir-authguard.appspot.com",
+    messagingSenderId: "529291880647",
+    appId: "1:529291880647:web:e56d4e88ed678b0ea076e2"
+  }
 };
